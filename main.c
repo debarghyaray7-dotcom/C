db4e8b3353047e6b985ce1da8cf8df4d80c15c9a
Debarghya Ray <debarghyaray7@gmail.com>
2025-09-06 12:16:06 +0530

    Add files via upload
    Made snake, water and gun game with C language

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(0));
+    int player, computer = rand() % 3;
+    printf("Choose 0 for Snake, 1 for water and 2 for Gun \n");
+    scanf("%d", &player);
+    printf("%d", computer);
+
+    if (player == 0 && computer == 0)
+        printf("Its a Draw! \n");
+    else if (player == 0 && computer == 1)
+        printf("You Win! \n");
+    else if (player == 0 &&computer == 2)
+        printf("You Lose\n");
+    else if (player == 1 &&computer == 0)
+        printf("You Lose! \n");
+    else if (player == 1 && computer == 1)
+        printf("Its a Draw!\n");
+    else if (player == 1 && computer == 2)
+        printf("You win!\n");
+    else if (player == 2 && computer == 0)
+        printf("You win!\n");
+    else if (player == 2 &&computer == 1)
+        printf("You Lose! \n");
+    else if (player == 2 && computer == 2)
+        printf("Its a Draw! \n");
+    return 0;
+}
